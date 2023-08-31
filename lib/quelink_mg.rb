abd4221b211854fd41aa3cb95b1d355f3e0824d5
Michał Dobosz <doboszmichal@o2.pl>
2023-08-31 07:10:40 +0200

    perf: add path

--- lib/quelink_mg.rb
+++ lib/quelink_mg.rb
@@ -8,10 +8,32 @@ require 'active_support/core_ext/time/zones'
 
 require File.expand_path('quelink-mg/at/base.rb', __dir__)
 require File.expand_path('quelink-mg/at/gtbsi.rb', __dir__)
+require File.expand_path('quelink-mg/at/gtcfg.rb', __dir__)
+require File.expand_path('quelink-mg/at/gtcmd.rb', __dir__)
+require File.expand_path('quelink-mg/at/gtfri.rb', __dir__)
+require File.expand_path('quelink-mg/at/gtqss.rb', __dir__)
+require File.expand_path('quelink-mg/at/gtrto.rb', __dir__)
+require File.expand_path('quelink-mg/at/gtsri.rb', __dir__)
+require File.expand_path('quelink-mg/at/gtudf.rb', __dir__)
+
 require File.expand_path('quelink-mg/resp/base.rb', __dir__)
 require File.expand_path('quelink-mg/resp/gtfri.rb', __dir__)
+require File.expand_path('quelink-mg/resp/gtgsv.rb', __dir__)
+require File.expand_path('quelink-mg/resp/gtinf.rb', __dir__)
+require File.expand_path('quelink-mg/resp/gtsos.rb', __dir__)
+require File.expand_path('quelink-mg/resp/gtstt.rb', __dir__)
+require File.expand_path('quelink-mg/resp/gtupc.rb', __dir__)
+
 require File.expand_path('quelink-mg/ack/base.rb', __dir__)
 require File.expand_path('quelink-mg/ack/gtbsi.rb', __dir__)
+require File.expand_path('quelink-mg/ack/gtcfg.rb', __dir__)
+require File.expand_path('quelink-mg/ack/gtcmd.rb', __dir__)
+require File.expand_path('quelink-mg/ack/gtfri.rb', __dir__)
+require File.expand_path('quelink-mg/ack/gtqss.rb', __dir__)
+require File.expand_path('quelink-mg/ack/gtrto.rb', __dir__)
+require File.expand_path('quelink-mg/ack/gtsri.rb', __dir__)
+require File.expand_path('quelink-mg/ack/gtudf.rb', __dir__)
+
 require File.expand_path('quelink-mg/configuration.rb', __dir__)
 
 module QuelinkMg
